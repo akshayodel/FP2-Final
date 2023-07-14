cca4d0eab5987aa06bf7ee3fda433994d460c59f
akshayodel <94823466+akshayodel@users.noreply.github.com>
2023-07-15 04:37:52 +0530

    Update FP2-Final.py

--- FP2-Final.py
+++ FP2-Final.py
@@ -1,197 +1,119 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
-#pip install streamlit
-
-
-# In[8]:
-
-
-#pip install requests
-
-
-# In[10]:
-
-
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from statsmodels.tsa.arima.model import ARIMA
-from dateutil.relativedelta import relativedelta
+import requests
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from statsmodels.tsa.arima.model import ARIMA
+import matplotlib.pyplot as plt
 
 # API URL
 api_url = "https://api.eia.gov/v2/international/data/?api_key=PYTvNry19FP8DgMdBAikT14IOtP21vBOZY2ZuDTJ&frequency=monthly&data[0]=value&facets[activityId][]=1&facets[productId][]=53&facets[countryRegionId][]=IND&facets[unit][]=TBPD&start=1990-01&end=2023-03&sort[0][column]=period&sort[0][direction]=asc&offset=0&length=5000"
 
-
-# In[14]:
-
-
-import requests
-
-
-# In[15]:
-
-
-#Read API data into a DataFrame
+# Send GET request to the API
 response = requests.get(api_url)
+
+# Check if the request was successful (status code 200)
 if response.status_code == 200:
+    # Get the response data
     data = response.json()
     df = pd.DataFrame(data['response']['data'])
-    df['ds'] = pd.to_datetime(df['period'])
-    df['y'] = df['value']
-    data_series = df[['ds', 'y']]
+else:
+    st.error("Failed to retrieve data. Status code: " + str(response.status_code))
 
+# Convert 'period' column to datetime format and assign it to 'ds' column
+df['ds'] = pd.to_datetime(df['period'])
 
-# In[16]:
+# Assign 'value' column to 'y' column
+df['y'] = df['value']
 
-
+# Create a new DataFrame with columns 'ds' and 'y'
+data_series = df[['ds', 'y']]
 
 API_TOKEN = 'a122af78b08f7eccfe99605fe17fb2050d3116e1'
 
-# Read CPI data from the specified URL
+# Read CPI data from the specified URL and parse the 'Date' column as dates
 df_cpi = pd.read_csv(
     'https://www.econdb.com/api/series/CPIIN/?token=%s&format=csv' % API_TOKEN,
     parse_dates=['Date'])
 
-# Merge the CPI dataframe and the data series dataframe
+# Merge the CPI dataframe (df_cpi) and the data series dataframe (data_series)
+# based on the common columns 'Date' and 'ds', using an inner join
 merged_df = df_cpi.merge(data_series, left_on='Date', right_on='ds', how='inner')
 
-# Set the cutoff period for training
-period = len(merged_df) - 6
-
-# Select the training data
-train_data = merged_df[['ds', 'CPIIN', 'y']][:period]
-
-# Select the test data
-test_data = merged_df[['ds', 'CPIIN', 'y']][period:]
-
 # Define the ARIMA order
-order = (1, 0, 0)
+order = (1, 0, 0)  # ARIMA order
 
-# Create and fit the ARIMAX model
-model_arimax = ARIMA(train_data['y'], exog=train_data['CPIIN'], order=order)
+# Create an ARIMAX model with the specified order, using 'y' as the endogenous variable
+# and 'CPIIN' as the exogenous variable in the training data
+model_arimax = ARIMA(merged_df['y'], exog=merged_df['CPIIN'], order=order)
+
+# Fit the ARIMAX model to the training data
 model_arimax_fit = model_arimax.fit()
 
+# Streamlit App
+st.title("Oil Production Forecast")
 
-# In[17]:
+# Select the test start date
+start_date = st.date_input("Select Test Start Date", value=(datetime.now() - relativedelta(months=5)).date())
 
+# Select the test end date
+end_date = st.date_input("Select Test End Date", value=(datetime.now() + relativedelta(months=4)).date())
 
+# Calculate the difference in months between the end date and start date
+months_diff = (end_date.year - start_date.year) * 12 + (end_date.month - start_date.month) + 1
 
+# Calculate the difference in months between the end date and start date for short-term and long-term durations
+short_term_duration = 6
+long_term_duration = 60
 
-# Define the prediction function
-def predict_oil_production(months_diff):
-    # Predict CPIIN
-    predicted_cpiin = model_arimax_fit.predict(start=len(data), end=len(data) + months_diff - 1)
-    predicted_cpiin = pd.DataFrame(predicted_cpiin, columns=['CPIIN'])
-    
-    # Predict oil production using the trained ARIMAX model
-    exog_reshaped = np.array(predicted_cpiin['CPIIN']).reshape(-1, 1)
-    
-    # Check if exog_reshaped is empty
-    if len(exog_reshaped) == 0:
-        raise ValueError('No exogenous values available for prediction.')
-    
-    predicted_values = model_arimax_fit.predict(start=len(merged_df)-6, end=len(merged_df)-6 + months_diff - 1, exog=exog_reshaped)
-    predicted_values = pd.DataFrame(predicted_values, columns=['Oil_Production'])
-    
-    # Generate monthly dates
-    start_date = merged_df['ds'].dt.date.max() - relativedelta(months=5)
-    end_date = datetime.today().date() + relativedelta(months=4)
+# Function to predict oil production
+def predict_oil_production(months_diff, title):
+    # Predicting CPIIN
+    predicted_value_cpiin = pd.DataFrame(model_fit.predict(start=len(merged_df), end=len(merged_df) + months_diff - 1))
+    predicted_value_cpiin['CPIIN'] = predicted_value_cpiin['predicted_mean']
+    # Create a new index
+    new_index = range(len(merged_df), len(merged_df) + months_diff)
+    # Set the new index using set_index()
+    predicted_value_cpiin.index = new_index
+
+    # Predicting oil production
+    exog_reshaped = predicted_value_cpiin['CPIIN'].values.reshape(-1, 1)
+    # Make predictions using the trained ARIMAX model
+    predicted_values = pd.DataFrame(model_arimax_fit.predict(start=len(merged_df) - 6, end=len(merged_df) - 6 + months_diff - 1, exog=exog_reshaped))
+    predicted_values['Oil_Production'] = predicted_values['predicted_mean'].round(2)
+    # Reset the index and start from 0
+    predicted_values.reset_index(drop=True, inplace=True)
+
+    # Generate the monthly dates within the specified range
     dates = pd.date_range(start=start_date, end=end_date, freq='MS')
-    
-    # Create a DataFrame with the predicted values and dates
-    predicted_df = pd.DataFrame({'Month': dates})
-    predicted_df = pd.concat([predicted_df, predicted_values], axis=1)
-    
-    return predicted_df
+    # Create a DataFrame with the month column
+    predicted_values['Month'] = pd.DataFrame({'Month': dates})
 
+    predicted_values = predicted_values.drop(columns=["predicted_mean"])
+    predicted_values = predicted_values[['Month', 'Oil_Production']]
 
-# def predict_oil_production(months_diff):
-#     # Predict CPIIN
-#     predicted_cpiin = model_arimax_fit.predict(start=len(data), end=len(data) + months_diff - 1)
-#     predicted_cpiin = pd.DataFrame(predicted_cpiin, columns=['CPIIN'])
-    
-#     # Predict oil production using the trained ARIMAX model
-#     exog_reshaped = np.array(predicted_cpiin['CPIIN']).reshape(-1, 1)
-#     #exog_reshaped = predicted_cpiin['CPIIN'].values.reshape(-1, 1)
-#     predicted_values = model_arimax_fit.predict(start=len(merged_df)-6, end=len(merged_df)-6 + months_diff - 1, exog=exog_reshaped)
-#     predicted_values = pd.DataFrame(predicted_values, columns=['Oil_Production'])
-    
-#     # Generate monthly dates
-#     start_date = merged_df['ds'].dt.date.max() - relativedelta(months=5)
-#     end_date = datetime.today().date() + relativedelta(months=4)
-#     dates = pd.date_range(start=start_date, end=end_date, freq='MS')
-    
-#     # Create a DataFrame with the predicted values and dates
-#     predicted_df = pd.DataFrame({'Month': dates})
-#     predicted_df = pd.concat([predicted_df, predicted_values], axis=1)
-    
-#     return predicted_df
+    st.write(predicted_values.to_string(index=False))
 
-# Create Streamlit widgets
-st.header('Oil Production Forecasting')
-
-start_date = st.date_input('Select Test Start Date', value=merged_df['ds'].dt.date.max() - relativedelta(months=5))
-end_date = st.date_input('Select Test End Date', value=datetime.today().date() + relativedelta(months=4))
-button_predict = st.button('Predict')
-
-# if button_predict:
-#     date_diff = end_date - start_date
-#     months_diff = (end_date.year - start_date.year) * 12 + (end_date.month - start_date.month) + 1
-    
-#     # Predict for the specified duration
-#     st.subheader('Oil Production Forecast: Custom Date')
-    
-#     try:
-#         prediction_custom = predict_oil_production(months_diff)
-#         st.dataframe(prediction_custom)
-#     except ValueError as e:
-#         st.error(str(e))
-#         st.stop()
-
-# Define the main Streamlit app logic
-if button_predict:
-    date_diff = end_date - start_date
-    months_diff = (end_date.year - start_date.year) * 12 + (end_date.month - start_date.month) + 1
-    
-    # Predict for the specified duration
-    st.subheader('Oil Production Forecast: Custom Date')
-    prediction_custom = predict_oil_production(months_diff)
-    st.dataframe(prediction_custom)
-    
-    # Predict for the short-term duration (6 months)
-    st.subheader('Oil Production Forecast: Short-Term Duration')
-    prediction_short_term = predict_oil_production(6)
-    st.dataframe(prediction_short_term)
-    
-    # Predict for the long-term duration (60 months)
-    st.subheader('Oil Production Forecast: Long-Term Duration')
-    prediction_long_term = predict_oil_production(60)
-    st.dataframe(prediction_long_term)
-    
-    # Reset the end date
-    end_date = start_date + relativedelta(days=date_diff.days)
-    
-    # Plot the oil production forecast
-    st.subheader('Oil Production Forecast: Graph')
+    # Plot the line graph
     plt.figure(figsize=(8, 4))
-    plt.plot(prediction_custom['Month'], prediction_custom['Oil_Production'], label='Custom Date')
-    plt.plot(prediction_short_term['Month'], prediction_short_term['Oil_Production'], label='Short-Term Duration')
-    plt.plot(prediction_long_term['Month'], prediction_long_term['Oil_Production'], label='Long-Term Duration')
+    plt.plot(predicted_values['Month'], predicted_values['Oil_Production'])
     plt.xlabel('Month')
     plt.ylabel('Oil Production')
-    plt.title('Oil Production Forecast')
-    plt.legend()
-    st.pyplot(plt)
+    plt.title(title)
+    plt.xticks(rotation=45)
+    st.pyplot()
 
+# Predict for custom date range
+st.header("Oil Production Forecast: Custom Date")
+predict_oil_production(months_diff, "Oil Production Forecast: Custom Date")
 
-# In[ ]:
+# Predict for short-term duration
+end_date_short_term = start_date + relativedelta(months=short_term_duration)
+st.header("Oil Production Forecast: Short-Term Duration")
+predict_oil_production(short_term_duration, "Oil Production Forecast: Short-Term Duration")
 
-
-
-
+# Predict for long-term duration
+end_date_long_term = start_date + relativedelta(months=long_term_duration)
+st.header("Oil Production Forecast: Long-Term Duration")
+predict_oil_production(long_term_duration, "Oil Production Forecast: Long-Term Duration")
